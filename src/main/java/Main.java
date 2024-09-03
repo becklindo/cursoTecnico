0d5ff5a7c33fe4f790e984f70131790525053207
Beck <169068547+becklindo@users.noreply.github.com>
2024-09-03 01:07:25 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+  public static void main(String[] args); {
+    System.out.println("#######");
+      System.out.println("curso técnico");
+    System.out.println("#######");
+    System.out.println("#O curso e muito legal, as matérias são mais difíceis e etc mas vão ajudar muito no meu futuro#");
+      System.out.println("As matérias do curso são bem interessantes, acho que vai ser bom para mim");
+      System.out.println( "Bom, não tenho mais oque falar então vou ficar enrolando aqui");
+      System.out.println("se estiver errado tudo oque estou fazendo me desculpa, eu não sei mexer com java, e nem esses códigos#");
+
+  
